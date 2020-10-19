89bed519febf62f3894a02f299f72dff85750abc
Thieffen Delabaere <Thieffen@users.noreply.github.com>
2020-10-19 23:22:38 +0200

    create basic app with User and Profile models

---
+++ api/prisma/seeds.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-console */
+const {PrismaClient} = require('@prisma/client')
+const dotenv = require('dotenv')
+
+dotenv.config()
+const db = new PrismaClient()
+
+async function main() {
+  // Seed data is database data that needs to exist for your app to run.
+  // Ideally this file should be idempotent: running it multiple times
+  // will result in the same database state (usually by checking for the
+  // existence of a record before trying to create it).
+
+  console.log(process.env.SEEDING_MESSAGE)
+
+  const existing = await db.user.findMany({where: {email: process.env.DEFAULT_USER_ADMIN_EMAIL}})
+
+  if (!existing.length) {
+    await db.user.create({
+      data: {
+        email: process.env.DEFAULT_USER_ADMIN_EMAIL,
+        password: process.env.DEFAULT_USER_ADMIN_PASSWORD
+      }
+    })
+  }
+
+}
+
+main()
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await db.$disconnect()
+  })
